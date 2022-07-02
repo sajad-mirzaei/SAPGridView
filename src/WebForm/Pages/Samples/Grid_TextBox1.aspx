fc9abbd0672e75d16532a381f35ab89177bace11
Sajad <mirzaei.sajad@hotmail.com>
2022-07-02 10:26:37 +0430

    Added Grid_TextBox sample

---
+++ src/WebForm/Pages/Samples/Grid_TextBox1.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Title="گزارش تست" Language="C#" MasterPageFile="~/MainBoard.master" AutoEventWireup="true" CodeFile="Grid_TextBox1.aspx.cs" Inherits="Grid_TextBox1" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="cphMain" runat="Server">
+    <ajax:UpdatePanel runat="server" ID="upForm" RenderMode="block" UpdateMode="Always">
+        <ContentTemplate>
+            <ajax:UpdateProgress class="loading" ID="UpdateProgressNoeLevelEtebarMoshtary" runat="server">
+                <ProgressTemplate>
+                    <asp:Label ID="Label001" Text="" runat="server"></asp:Label>
+                    <img alt="لطفا چند لحظه صبر کنید..." src="../../../Assets/Styles/images/wait.gif" />
+                </ProgressTemplate>
+            </ajax:UpdateProgress>
+
+            <div class="FormBox">
+                <div class="HeaderBox">
+                    Test1
+                </div>
+                <div class="ContentBox">
+                    <div id="MyGridId"></div>
+                </div>
+            </div>
+            <script type="text/javascript">
+                function updateRowClient(oData) {
+                    //console.log(oData);
+                    var tableId = oData.CallBackData.RowData["tableId"];
+                    var textbox1Value = $("#textbox1-" + tableId).val();
+                    var inputData = JSON.stringify({
+                        textbox1Value: textbox1Value
+                    });
+                    var rowData = JSON.stringify(oData.CallBackData.RowData);
+                    $.ajax({
+                        type: "POST",
+                        url: document.location.origin + document.location.pathname + "/updateRowServer",
+                        data: "{ inputData: '" + inputData + "', rowData: '" + rowData + "'}",
+                        contentType: "application/json; charset=utf-8",
+                        dataType: "json",
+                        success: function (data) {
+                            console.log(data);
+                        },
+                        error: function (error) {
+                            console.log(error);
+                        }
+                    });
+                }
+            </script>
+        </ContentTemplate>
+    </ajax:UpdatePanel>
+</asp:Content>
---
+++ src/WebForm/Pages/Samples/Grid_TextBox1.aspx.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using SAP.WebControls;
+using System.Data;
+using System.Web.Services;
+using Newtonsoft.Json;
+
+public partial class Grid_TextBox1 : System.Web.UI.Page
+{
+    public static SAPGridView oSGV = new SAPGridView();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        DataTable dt = MakeDataTable();
+
+        oSGV.Grids["MyGrid1"] = new Grid()
+        {
+            ContainerId = "MyGridId",
+            ContainerHeight = 400,
+            Data = dt,
+            Columns = new List<Column>() {
+                new Column { Data = "y", Title = "Name 0", DefaultContent = "",
+                    Functions = { 
+                        new TextFeature {
+                            Section = Function.SectionValue.Tbody,
+                            Condition = "1==1",
+                            IsTrueText = "<input type='textbox' name='textbox1' id='textbox1-tableId' value='x1' data-value1='tableId' data-value2='x2'>",
+                            IsFalseText = "",
+                        }
+                    }
+                },
+                new Column { Data = "xCustom", Title = "Submit", DefaultContent = "<span class='btn btn-primary btn-sm'>edit</span>", 
+                    Functions = {
+                        new OnClick { 
+                            Section = Function.SectionValue.Tbody, 
+                            JavaScriptMethodName = "updateRowClient", 
+                            HttpRequestType = OnClick.HttpRequestTypeValue.CallJavaScriptMethod 
+                        }
+                    }
+                },
+                new Column { Data = "x1", Title = "Name 1" },
+                new Column { Data = "x2", Title = "Name 2" },
+                new Column { Data = "x3", Title = "Name 3" },
+                new Column { Data = "x4", Title = "Name 4" }
+            }
+        };
+
+        oSGV.GridBind("MyGrid1");
+    }
+
+    [WebMethod]
+    public static string updateRowServer(string inputData, string rowData)
+    {
+        Dictionary<string, string> inputDataArray = JsonConvert.DeserializeObject<Dictionary<string, string>>(inputData);
+        Dictionary<string, string> rowDataArray = JsonConvert.DeserializeObject<Dictionary<string, string>>(rowData);
+        return "ok";
+    }
+
+    public DataTable MakeDataTable()
+    {
+        DataTable oDT = new DataTable();
+        oDT.Columns.Add("tableId", typeof(int));
+        oDT.Columns.Add("x1", typeof(int));
+        oDT.Columns.Add("x2", typeof(string));
+        oDT.Columns.Add("x3", typeof(string));
+        oDT.Columns.Add("x4", typeof(string));
+
+        for (int i = 0; i < 10; i++)
+        {
+            DataRow Row1 = oDT.NewRow();
+            Row1["tableId"] = i + 1;
+            Row1["x1"] = i;
+            Row1["x2"] = i + 1;
+            Row1["x3"] = i + 2;
+            Row1["x4"] = i + 3;
+            oDT.Rows.Add(Row1);
+        }
+        return oDT;
+    }
+
+}
