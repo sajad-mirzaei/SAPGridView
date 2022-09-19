46e09792aff6d449c39e9946f8610a2a1d4efd6a
Sajad <mirzaei.sajad@hotmail.com>
2022-09-19 12:39:40 +0430

    Added TextBox2 send textBox value per row

---
+++ src/WebForm/Pages/Samples/Grid_TextBox2.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Title="گزارش تست" Language="C#" MasterPageFile="~/MainBoard.master" AutoEventWireup="true" CodeFile="Grid_TextBox2.aspx.cs" Inherits="Grid_TextBox2" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="cphMain" runat="Server">
+    <ajax:UpdatePanel runat="server" ID="upForm" RenderMode="block" UpdateMode="Always">
+        <ContentTemplate>
+            <ajax:UpdateProgress class="loading" ID="UpdateProgressNoeLevelEtebarMoshtary" runat="server">
+                <ProgressTemplate>
+                    <asp:Label ID="Label001" Text="" runat="server"></asp:Label>
+                    <img alt="لطفا چند لحظه صبر کنید..." src="../../../Assets/Styles/images/wait.gif" />
+                </ProgressTemplate>
+            </ajax:UpdateProgress>
+
+            <asp:HiddenField ID="HiddenField1" runat="server" />
+            <asp:LinkButton ID="LinkButtonHidden" runat="server" CssClass="d-none" OnClick="LinkButtonHidden_Click">LinkButtonHidden</asp:LinkButton>
+        </ContentTemplate>
+    </ajax:UpdatePanel>
+    <div class="FormBox">
+        <div class="HeaderBox">
+            Test1 <i class='fa fa-solid fa-filter-circle-xmark'></i>
+        </div>
+        <div class="ContentBox">
+            <div id="MyGridId"></div>
+        </div>
+    </div>
+    <script>
+        function FunctionTest1(id) {
+            var v = $("#TextBox-" + id).val();
+            $("#<%=HiddenField1.ClientID%>").val(v);
+            __doPostBack('ctl00$cphMain$LinkButtonHidden', '');
+        }
+    </script>
+</asp:Content>
---
+++ src/WebForm/Pages/Samples/Grid_TextBox2.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using SAP.WebControls;
+using System.Data;
+using System.Web.UI.WebControls;
+
+public partial class Grid_TextBox2 : System.Web.UI.Page
+{
+    public static SAPGridView oSGV = new SAPGridView();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        DataTable dt = MakeDataTable();
+
+        oSGV.Grids["MyGrid1"] = new Grid()
+        {
+            ContainerId = "MyGridId",
+            ContainerHeight = 400,
+            Data = dt,
+            Columns =
+            {
+                new Column { Data = "a", Title = "AA"},
+                new Column { Data = "Test1", Title = "Test1", DefaultContent = "",
+                    Functions = { 
+                        new TextFeature { 
+                            Section = Function.SectionValue.Tbody,
+                            Condition = "1==1",
+                            IsTrueText = "<input type='text' id='TextBox-a' value='b' /> " + " <span onclick='FunctionTest1(a)' href='#' id='a' class='btn btn-primary LinkButton'>Edit</span>"
+                        }
+                    }
+                }
+            }
+        };
+
+        oSGV.GridBind("MyGrid1");
+    }
+
+
+    public DataTable MakeDataTable()
+    {
+        DataTable oDT = new DataTable();
+        oDT.Columns.Add("a", typeof(int));
+        oDT.Columns.Add("b", typeof(string));
+        oDT.Columns.Add("c", typeof(string));
+        oDT.Columns.Add("d", typeof(string));
+
+        for (int i = 0; i < 100; i++)
+        {
+            DataRow Row1 = oDT.NewRow();
+            Row1["a"] = i;
+            Row1["b"] = "bb " + i;
+            Row1["c"] = "cc " + i;
+            Row1["d"] = "dd " + i;
+
+            oDT.Rows.Add(Row1);
+        }
+        return oDT;
+    }
+
+
+    protected void LinkButtonHidden_Click(object sender, EventArgs e)
+    {
+        var h = HiddenField1.Value;
+    }
+}
