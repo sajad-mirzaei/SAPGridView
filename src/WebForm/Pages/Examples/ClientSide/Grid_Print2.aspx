ad232a2289ed180659dfdc1dd1c43806b03ebe50
mirzaei.sajad@hotmail.com <mirzaei.sajad@hotmail.com>
2024-01-13 19:26:46 +0330

    Added Grid_Print2

---
+++ src/WebForm/Pages/Examples/ClientSide/Grid_Print2.aspx
@@ -0,0 +1,70 @@
+﻿<%@ Page Title="گزارش تست" Language="C#" MasterPageFile="~/MainBoard.master" AutoEventWireup="true" CodeFile="Grid_Print2.aspx.cs" Inherits="Grid_Print2" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="cphMain" runat="Server">
+    <ajax:UpdatePanel runat="server" ID="upForm" RenderMode="block" UpdateMode="Always">
+        <ContentTemplate>
+            <ajax:UpdateProgress class="loading" ID="UpdateProgressNoeLevelEtebarMoshtary" runat="server">
+                <ProgressTemplate>
+                    <asp:Label ID="Label001" Text="" runat="server"></asp:Label>
+                    <img alt="لطفا چند لحظه صبر کنید..." src="../../../Assets/Styles/images/wait.gif" />
+                </ProgressTemplate>
+            </ajax:UpdateProgress>
+
+            <div class="FormBox">
+                <div class="HeaderBox">
+                    Test1 <i class='fa fa-solid fa-filter-circle-xmark'></i>
+                </div>
+                <div class="ContentBox">
+                    <div id="MyGridId"></div>
+                </div>
+            </div>
+            <script>
+                function MyPrintButtonMethod(oData) {
+                    var orientation = {
+                        portrait: "200mm",
+                        landscape: "290mm"
+                    };
+                    var printWith = orientation.portrait;
+                    var btnOptions = {
+                        extend: 'print',
+                        titleAttr: "پرینت11",
+                        autoPrint: false,
+                        text: '<i class="fa fa-print DataTableIcons"></i>',
+                        footer: true,
+                        title: "",
+                        //customize main data grid
+                        customize: function (win, obj, options) {
+                            var dataGrid = $(win.document.body).find('.Grid');
+                            dataGrid.attr("class", "").css("width", printWith).css('font-size', 'inherit')
+                            dataGrid.find("thead").css("background-color", "#dddddd !important");
+                            dataGrid.find("tbody").find("tr:odd").css("background-color", "#eeeeee !important");
+                        },
+                        //customize report info
+                        messageTop: function () {
+                            var str = "<style>";
+                            str += "@page { size: A4; margin: 5mm; width: " + printWith + "; }";
+                            str += "@media print { html, body { width: " + printWith + "; height: 290mm; } }";
+                            str += "td, th { border: 1px solid #dddddd; padding: 5px; }";
+                            str += ".headerTable { width: calc(100% - 116px) !important; height: 3cm; border: 1px solid #dddddd; float: right; }";
+                            str += ".mainPage { width: " + printWith + " !important; position: relative; }";
+                            str += ".logoImg { height: 111px; width: 114px; }";
+                            str += "</style>";
+                            str += '<div class="mainPage">';
+                            str += '<table class="headerTable">';
+                            str += '<tr><td colspan="2" class="text-center">' + oData.YourData.sherkatName + '</td></tr>';
+                            str += '<tr><td> گزارش گیرنده: ' + oData.YourData.user + '</td> <td> نام گزارش: ' + oData.YourData.reportName + '</td></tr>';
+                            str += '<tr><td> از تاریخ: ' + oData.YourData.fromDate + '</td> <td> تا تاریخ: ' + oData.YourData.toDate + '</td></tr>';
+                            str += '</table>';
+                            str += '<div style="float:left; border: 1px solid #dddddd;"><img class="logoImg" src="' + oData.YourData.logo + '" /></div>';
+                            str += '</div>';
+                            return str;
+
+                        }
+                    };
+                    return btnOptions;
+                }
+            </script>
+
+        </ContentTemplate>
+    </ajax:UpdatePanel>
+</asp:Content>
---
+++ src/WebForm/Pages/Examples/ClientSide/Grid_Print2.aspx.cs
@@ -0,0 +1,117 @@
+﻿using SAP.WebControls;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+public partial class Grid_Print2 : System.Web.UI.Page
+{
+    public static SAPGridView oSGV = new SAPGridView();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        DataTable dt = MakeDataTable();
+        var customizeButtonData = new Dictionary<string, string>() {
+            { "sherkatName", "نام شرکت" },
+            { "user", "سجاد میرزایی" },
+            { "reportName", "گزارش تست 1" },
+            { "logo", "https://delpazirerp.com/SAP/Assets/Styles/images/logoSAP.png" },
+            { "fromDate", "2022/01/01" },
+            { "toDate", "2022/01/13" }
+        };
+
+        oSGV.Grids["MyGrid1"] = new Grid()
+        {
+            ContainerId = "MyGridId",
+            ContainerHeight = 400,
+            Data = dt,
+            GridTitle = "گزارش تست 1",
+
+            CustomizeButtons = new List<CustomizeButton>() {
+                new CustomizeButton{
+                    ButtonName = ButtonNames.Print,
+                    JavascriptMethodName = "MyPrintButtonMethod",
+                    Data = customizeButtonData
+                }
+            },
+
+            Options = new Option() { DropDownFilterButton = true, TitleRowInExelExport = false },
+            Columns = new List<Column>() {
+                new Column { Data = "v1", Title = "vv1", DefaultContent = "vv2", Visible = false },
+                new Column { Data = "a", Title = "aa",
+                    Functions = {
+                        new Calc { Section = Function.SectionValue.Tfoot, Operator = Calc.OperatorValue.VerticalSum },
+                        new Separator { Section = Function.SectionValue.Tfoot }
+                    }
+                },
+                new Column { Data = "v2", Title = "vv2", DefaultContent = "vv2", Visible = false },
+                new Column { Data = "CumulativeTest1", Title = "CumulativeSum(aa)", DefaultContent = "", CssClass = "ltr",
+                    Functions = {
+                        new CumulativeSum { Section = Function.SectionValue.Tbody, SourceField = "a" }
+                    }
+                },
+                new Column { Data = "b", Title = "bb" },
+                new Column { Data = "c", Title = "cc",
+                    Functions = {
+                        new Calc { Section = Function.SectionValue.Tfoot, Operator = Calc.OperatorValue.VerticalSum }
+                    }
+                },
+                new Column { Data = "d", Title = "dd" },
+                new Column { Data = "e", Title = "ee",
+                    Functions = {
+                        new Calc { Section = Function.SectionValue.Tfoot, Operator = Calc.OperatorValue.VerticalSum }
+                    }
+                },
+                new Column { Data = "f", Title = "ff" }
+            }
+        };
+
+        oSGV.GridBind("MyGrid1");
+    }
+
+
+    public DataTable MakeDataTable()
+    {
+        DataTable oDT = new DataTable();
+        oDT.Columns.Add("a", typeof(int));
+        oDT.Columns.Add("b", typeof(string));
+        oDT.Columns.Add("c", typeof(string));
+        oDT.Columns.Add("d", typeof(string));
+        oDT.Columns.Add("e", typeof(string));
+        oDT.Columns.Add("f", typeof(string));
+
+        for (int i = 0; i < 100; i++)
+        {
+            DataRow Row1 = oDT.NewRow();
+            Row1["a"] = i + 1000;
+            Row1["b"] = "bb " + i;
+            Row1["c"] = i + 1;
+
+            if (i % 2 == 0)
+                Row1["d"] = "کسر موجودي";
+            else
+                Row1["d"] = "تغيير قيمت";
+
+            if (i < 3) Row1["e"] = 3;
+            else if (i < 6) Row1["e"] = 6;
+            else if (i < 9) Row1["e"] = 9;
+            else if (i < 12) Row1["e"] = 12;
+            else if (i < 15) Row1["e"] = 15;
+            else if (i < 18) Row1["e"] = 18;
+            else if (i < 21) Row1["e"] = 21;
+            else Row1["e"] = 22;
+
+
+            if (i < 8) Row1["f"] = "ff1";
+            else if (i < 16) Row1["f"] = "ff2";
+            else if (i < 24) Row1["f"] = "ff3";
+            else if (i < 32) Row1["f"] = "ff4";
+            else if (i < 40) Row1["f"] = "ff5";
+            else if (i < 48) Row1["f"] = "ff6";
+            else if (i < 56) Row1["f"] = "ff7";
+            else Row1["f"] = "ff8";
+
+            oDT.Rows.Add(Row1);
+        }
+        return oDT;
+    }
+
+}
