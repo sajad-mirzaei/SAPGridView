29421d578ecf7a7f2950aa834d679a1abdbe8c10
mirzaei.sajad@hotmail.com <mirzaei.sajad@hotmail.com>
2024-05-21 16:58:47 +0330

    Added NestedGridsMultpleLevel

--- src/WebForm/Pages/Examples/ClientSide/NestedGridsMultpleLevel.aspx.cs
+++ src/WebForm/Pages/Examples/ClientSide/NestedGridsMultpleLevel.aspx.cs
@@ -2,7 +2,6 @@
 using SAP.WebControls;
 using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Linq;
 using System.Web.Services;
 using System.Web.UI;
@@ -13,199 +12,198 @@ public partial class NestedGridsMultpleLevel : Page
     {
         if (!Page.IsPostBack)
         {
-            var d11 = DateTime.Today.AddMonths(-10).ToString("yyyy-MM-dd");
-            var d22 = DateTime.Today.ToString("yyyy-MM-dd");
+            var mockData = new MockData().GetMockData();
+            var oSGV = CreatePersonInfoGrid(mockData);
+            oSGV.GridBind("PersonInfo");
         }
     }
 
-    protected void btnView_Click(object sender, EventArgs e)
-    {
-        var oSGV = CreateFirstGrid();
-        oSGV.GridBind("Grid1");
-    }
-
-    public SAPGridView CreateFirstGrid()
+    public SAPGridView CreatePersonInfoGrid(MockData mockData)
     {
         SAPGridView oSGV = new SAPGridView();
         oSGV.DefaultParameters = new Dictionary<string, string>()
         {
-            { "Level", "1" },
-            { "Id", "" }
+            { "Level", "1" }
         };
-        DataTable dt = Get_DataTable1(oSGV.DefaultParameters);
         //-----------------------Grid1------------------------
-        oSGV.Grids["Grid1"] = new Grid()
+        oSGV.Grids["PersonInfo"] = new Grid()
         {
             ContainerId = "MyGridId",
             ContainerHeight = 300,
-            GridTitle = "aaaa",
-            Options = new Option
-            {
-                DropDownFilterButton = true,
-                ExcelButton = true,
-                CopyButton = true,
-                PrintButton = true,
-                RecycleButton = true,
-                ColumnsSearchButton = true,
-                Info = true,
-                GridSearchTextBox = true
-            },
+            GridTitle = "GridTitle Test",
+            Data = mockData.PersonInfoList,
             Columns = new List<Column>() {
-                new Column { Title ="شماره ثبت", Data="Id" },
-                new Column { Title ="تاریخ", Data="Tarikh",
+                new Column { Title ="id", Data = "Id" },
+                new Column { Title ="First Name", Data = "FirstName" },
+                new Column { Title ="Last Name", Data = "LastName" },
+                new Column { Title ="Age", Data = "Age" },
+                new Column
+                {
+                    Title ="Academic Records",
+                    Data = "AcademicRecords",
+                    DefaultContent = "<i class='fa fa-list-alt'></i>",
+                    CssClass = "text-center",
                     Functions =
                     {
-                        new MiladiToJalali {Section = Calc.SectionValue.Tbody, Output = MiladiToJalali.DateValue.FullDate },
-                        new OnClick {
+                        new OnClick()
+                        {
                             Section = Function.SectionValue.Tbody,
-                            NextTabTitle = "Tarikh - {clickedItem}",
+                            NextGrid = "AcademicRecords",
                             Level = "2",
-                            NextGrid = "Grid2",
-                            DataKeys = { "Id" }
+                            DataKeys = { "Id" },
+                            NextTabTitle = "FirstName LastName Academic Records"
                         }
                     }
-                },
-                new Column { Title ="ستون 1", Data="Col1",
-                    Functions =
-                    {
-                        new Calc {Section = Calc.SectionValue.Tfoot, Operator = Calc.OperatorValue.VerticalSum },
-                        new Separator { Section = Separator.SectionValue.Tbody, DecimalPlaces = 0 },
-                        new Separator { Section = Separator.SectionValue.Tfoot, DecimalPlaces = 0 }
-                    }
-                },
-                new Column { Title ="ستون 2", Data="Col2" },
-                new Column { Title ="ستون 3", Data="Col3" }
+                }
             }
         };
-        //-----------------------CheckData--------------------
-        if (dt.Rows.Count > 0)
-        {
-            oSGV.Grids["Grid1"].Data = dt;
-            oSGV.Grids["Grid1"].GridParameters = new Dictionary<string, string>(oSGV.DefaultParameters);
-            oSGV.GridBind("Grid1");
-            Div_Grids.Visible = true;
-        }
-
         return oSGV;
     }
-
-    protected DataTable Get_DataTable1(Dictionary<string, string> param)
+    public static Grid CreateNextGrids(string nextGrid, Dictionary<string, string> filters)
     {
-        //در صورتیکه اطلاعات را از دیتابیس فراخوانی میکنید، نیازی به این متد نیست
-        DataTable dt = new DataTable();
-        dt.Columns.Add("Id", typeof(int));
-        dt.Columns.Add("Tarikh", typeof(DateTime));
-        dt.Columns.Add("Col1", typeof(string));
-        dt.Columns.Add("Col2", typeof(string));
-        dt.Columns.Add("Col3", typeof(string));
-
-        for (int i = 1; i <= 20; i++)
+        SAPGridView oSGV = new SAPGridView();
+        MockData mockData = new MockData().GetMockData();
+        //-----------------------AcademicRecords Grid------------------------
+        oSGV.Grids["AcademicRecords"] = new Grid()
         {
-            DataRow row = dt.NewRow();
-            row["Id"] = i;
-            row["Col1"] = i + 10000;
-            row["Col2"] = "Col2 - " + i;
-            row["Col3"] = "Col3 - " + i;
-            row["Tarikh"] = DateTime.Now.AddMonths(-1 * i);
-            dt.Rows.Add(row);
-        }
-        var result = dt
-                    .AsEnumerable()
-                    .Where(myRow => myRow.Field<DateTime>("Tarikh") >= DateTime.Parse(param["AzTarikh"]) && myRow.Field<DateTime>("Tarikh") <= DateTime.Parse(param["TaTarikh"]))
-                    .CopyToDataTable();
-
-        return result;
-    }
-
-    protected static DataTable Get_DataTable2(Dictionary<string, string> param)
-    {
-        //در صورتیکه اطلاعات را از دیتابیس فراخوانی میکنید، نیازی به این متد نیست
-        DataTable dt = new DataTable();
-        dt.Columns.Add("Id", typeof(int));
-        dt.Columns.Add("Tarikh", typeof(DateTime));
-        dt.Columns.Add("property1", typeof(string));
-        dt.Columns.Add("property2", typeof(string));
-        dt.Columns.Add("property3", typeof(string));
-
-        for (int i = 1; i <= 20; i++)
+            ContainerId = "MyGridId",
+            ContainerHeight = 300,
+            GridTitle = "GridTitle Test 2",
+            Columns = new List<Column>() {
+                new Column { Title ="id", Data = "Id" },
+                new Column { Title ="Degree", Data = "Degree" },
+                new Column { Title ="Major", Data = "Major" },
+                new Column { Title ="Institute", Data = "Institute" },
+                new Column
+                {
+                    Title ="Academic Record Details",
+                    Data = "AcademicRecordDetails",
+                    DefaultContent = "<i class='fa fa-list-alt'></i>",
+                    Functions =
+                    {
+                        new OnClick()
+                        {
+                            Section = Function.SectionValue.Tbody,
+                            NextGrid = "AcademicRecordDetails",
+                            Level = "3",
+                            DataKeys = { "Id" },
+                            NextTabTitle = "Degree Major Academic Records Details"
+                        }
+                    }
+                }
+            }
+        };
+        //-----------------------AcademicRecordDetails Grid------------------------
+        oSGV.Grids["AcademicRecordDetails"] = new Grid()
         {
-            DataRow row = dt.NewRow();
-            row["Id"] = i;
-            row["property1"] = i + 10000 * int.Parse(param["Id"]);
-            row["property2"] = "col" + param["Id"] + " - property2 - " + i;
-            row["property3"] = "col" + param["Id"] + " - property3 - " + i;
-            row["Tarikh"] = DateTime.Now.AddMonths(-1 * i);
-            dt.Rows.Add(row);
-        }
-        var result = dt
-                    .AsEnumerable()
-                    .Where(myRow => myRow.Field<DateTime>("Tarikh") >= DateTime.Parse(param["AzTarikh"]) && myRow.Field<DateTime>("Tarikh") <= DateTime.Parse(param["TaTarikh"]))
-                    .CopyToDataTable();
+            ContainerId = "MyGridId",
+            ContainerHeight = 300,
+            GridTitle = "GridTitle Test 3",
+            Columns = new List<Column>() {
+                new Column { Title ="id", Data = "Id" },
+                new Column { Title ="Start Date", Data = "StartDate" },
+                new Column { Title ="End Date", Data = "EndDate" },
+                new Column { Title ="Average", Data = "Average" }
+            }
+        };
 
-        return result;
+        var grid = oSGV.Grids[nextGrid];
+        if (nextGrid == "AcademicRecords")
+        {
+            grid.Data = mockData.AcademicRecordsList.Where(c => c.PersonInfoId == int.Parse(filters["Id"]));
+        }
+        else
+        {
+            grid.Data = mockData.AcademicRecordDetailsList.Where(c => c.AcademicRecordsId == int.Parse(filters["Id"]));
+        }
+        return grid;
     }
 
     [WebMethod]
     public static string SapGridEvent(string CallBackData)
     {
-        var oSGV = CreateStaticGrids();
         SapGridCallBackEvent oData = JsonConvert.DeserializeObject<SapGridCallBackEvent>(CallBackData);
         //--clicked row data-------------------------------------
-        var RowData = oData.RowData;
-        List<string> DataKeys = oData.FuncArray.DataKeys;
-        string NextGrid = oData.FuncArray.NextGrid;
-        string Clicked_CellName = oData.TableDetails["CellName"];
-        int Level = int.Parse(oData.FuncArray.Level);
+        var rowData = oData.RowData;
+        List<string> dataKeys = oData.FuncArray.DataKeys;
+        string nextGrid = oData.FuncArray.NextGrid;
+        string clicked_CellName = oData.TableDetails["CellName"];
+        int level = int.Parse(oData.FuncArray.Level);
 
-        //--copy last grid parameters into new grid parameters---
-        oSGV.Grids[NextGrid].GridParameters = new Dictionary<string, string>(oData.GridParameters);
-
-        //--change new grid parameters with new values-----------
-        oSGV.Grids[NextGrid].GridParameters["Level"] = oData.FuncArray.Level;
-        foreach (string DataKey in DataKeys)
+        var filters = new Dictionary<string, string>(oData.GridParameters);
+        foreach (string dataKey in dataKeys)
         {
-            if (RowData.Count != 0)
-                oSGV.Grids[NextGrid].GridParameters[DataKey] = RowData[DataKey];
+            if (rowData.Count != 0)
+                filters[dataKey] = rowData[dataKey];
         }
-        oSGV.Grids[NextGrid].Data = Get_DataTable2(oSGV.Grids[NextGrid].GridParameters);
-        return oSGV.AjaxBind(NextGrid);
-    }
 
-    public static SAPGridView CreateStaticGrids()
-    {
-        SAPGridView oSGV = new SAPGridView();
-        //-----------------------Grid2------------------------
-        oSGV.Grids["Grid2"] = new Grid()
-        {
-            ContainerId = "MyGridId",
-            ContainerHeight = 300,
-            Columns = new List<Column>() {
-                new Column { Title ="شماره ثبت", Data="Id" },
-                new Column { Title ="تاریخ", Data="Tarikh",
-                    Functions =
-                    {
-                        new MiladiToJalali {Section = Calc.SectionValue.Tbody, Output = MiladiToJalali.DateValue.FullDate }
-                    }
-                },
-                new Column { Title ="مشخصه 1", Data="property1",
-                    Functions =
-                    {
-                        new Calc {Section = Calc.SectionValue.Tfoot, Operator = Calc.OperatorValue.VerticalSum },
-                        new Separator { Section = Separator.SectionValue.Tbody, DecimalPlaces = 0 },
-                        new Separator { Section = Separator.SectionValue.Tfoot, DecimalPlaces = 0 }
-                    }
-                },
-                new Column { Title ="مشخصه 2", Data="property2" },
-                new Column { Title ="مشخصه 3", Data="property3" }
-            }
-        };
-        return oSGV;
+        var oSGV = new SAPGridView();
+        oSGV.Grids[nextGrid] = CreateNextGrids(nextGrid, filters);
+        oSGV.Grids[nextGrid].GridParameters = filters;
+        oSGV.Grids[nextGrid].GridParameters["Level"] = oData.FuncArray.Level;
+
+        return oSGV.AjaxBind(nextGrid);
     }
 }
 
-public class MultpleLevelModel1
+public class MockData
 {
-    public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
+    public List<PersonInfo> PersonInfoList { get; set; }
+    public List<AcademicRecords> AcademicRecordsList { get; set; }
+    public List<AcademicRecordDetails> AcademicRecordDetailsList { get; set; }
+    public class PersonInfo
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Age { get; set; }
+    }
+    public class AcademicRecords
+    {
+        public int Id { get; set; }
+        public string Degree { get; set; }
+        public string Major { get; set; }
+        public string Institute { get; set; }
+        public int PersonInfoId { get; set; }
+    }
+    public class AcademicRecordDetails
+    {
+        public int Id { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public string Average { get; set; }
+        public int AcademicRecordsId { get; set; }
+    }
+
+    public MockData GetMockData()
+    {
+        var mockData = new MockData();
+        mockData.PersonInfoList = new List<PersonInfo>
+        {
+            new PersonInfo { Id = 1, FirstName = "John", LastName = "Doe", Age = "30" },
+            new PersonInfo { Id = 2, FirstName = "Jane", LastName = "Smith", Age = "28" },
+            new PersonInfo { Id = 3, FirstName = "Alice", LastName = "Johnson", Age = "32" }
+        };
+
+        mockData.AcademicRecordsList = new List<AcademicRecords>
+        {
+            new AcademicRecords { Id = 1, Degree = "B.Sc.", Major = "Computer Science", Institute = "MIT", PersonInfoId = 1 },
+            new AcademicRecords { Id = 2, Degree = "M.Sc.", Major = "Computer Science", Institute = "Stanford", PersonInfoId = 1 },
+            new AcademicRecords { Id = 3, Degree = "B.A.", Major = "English Literature", Institute = "Harvard", PersonInfoId = 2 },
+            new AcademicRecords { Id = 4, Degree = "M.A.", Major = "English Literature", Institute = "Yale", PersonInfoId = 2 },
+            new AcademicRecords { Id = 5, Degree = "B.Sc.", Major = "Physics", Institute = "Caltech", PersonInfoId = 3 },
+            new AcademicRecords { Id = 6, Degree = "Ph.D.", Major = "Physics", Institute = "UC Berkeley", PersonInfoId = 3 }
+        };
+
+        mockData.AcademicRecordDetailsList = new List<AcademicRecordDetails>
+        {
+            new AcademicRecordDetails { Id = 1, StartDate = "2010-09-01", EndDate = "2014-06-15", Average = "20", AcademicRecordsId = 1 },
+            new AcademicRecordDetails { Id = 2, StartDate = "2014-09-01", EndDate = "2016-06-15", Average = "19", AcademicRecordsId = 2 },
+            new AcademicRecordDetails { Id = 3, StartDate = "2008-09-01", EndDate = "2012-06-15", Average = "18", AcademicRecordsId = 3 },
+            new AcademicRecordDetails { Id = 4, StartDate = "2012-09-01", EndDate = "2014-06-15", Average = "17", AcademicRecordsId = 4 },
+            new AcademicRecordDetails { Id = 5, StartDate = "2009-09-01", EndDate = "2013-06-15", Average = "16", AcademicRecordsId = 5 },
+            new AcademicRecordDetails { Id = 6, StartDate = "2013-09-01", EndDate = "2018-06-15", Average = "15", AcademicRecordsId = 6 }
+        };
+        return mockData;
+    }
 }
